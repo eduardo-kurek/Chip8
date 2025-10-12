af5974ef808c569b57fe70eb8426f8e8250b0b4b
ViniciusKurek <vinicius.kurek@hotmail.com>
2025-10-11 21:15:14 -0300

    Odeo você SDL

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// #include <SDL.h>
+#include <SDL.h>
 #include <iostream>
 #include "Context.h"
 
@@ -29,80 +29,105 @@
 #include "instructions/JMP_V0.h"
 #include "instructions/RND.h"
 
+void wait_for_input() {
+    std::cout << "Pressione ENTER para continuar..." << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
 int main(int argc, char *argv[]) {
-    CLI::App app{"Chip8 Interpreter"};
-    std::string romPath;
 
-    app.add_option("rom", romPath, "Caminho da ROM")->required();
-    app.parse(argc, argv);
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Erro SDL_Init: %s", SDL_GetError());
+        return -1;
+    }
 
-    Decoder decoder;
-    decoder.Register(0xFFFF, 0x00E0, Instruction::GetFactoryOf<CLS>());
-    decoder.Register(0xFFFF, 0x00EE, Instruction::GetFactoryOf<RET>());
+    SDL_Window* window = SDL_CreateWindow("Teste", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    if (!window) {
+        SDL_Log("Erro ao criar janela: %s", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
 
-    decoder.Register(0XF000, 0x1000, Instruction::GetFactoryOf<JMP>());
-    
-    decoder.Register(0xF000, 0x2000, Instruction::GetFactoryOf<CALL>());
-    
-    decoder.Register(0xF000, 0x3000, Instruction::GetFactoryOf<SE_N>());
+    SDL_Delay(5000); // Mantém a janela aberta por 5 segundos
 
-    decoder.Register(0xF000, 0x4000, Instruction::GetFactoryOf<SNE_N>());
-
-    decoder.Register(0xF000, 0x5000, Instruction::GetFactoryOf<SE_R>());
-    
-    decoder.Register(0xF000, 0x6000, Instruction::GetFactoryOf<LD_N>());
-    
-    decoder.Register(0xF000, 0x7000, Instruction::GetFactoryOf<ADD_N>());
-
-    decoder.Register(0xF00F, 0x8000, Instruction::GetFactoryOf<LD_R>());
-    decoder.Register(0xF00F, 0x8001, Instruction::GetFactoryOf<OR_R>());
-    decoder.Register(0xF00F, 0x8002, Instruction::GetFactoryOf<AND_R>());
-    decoder.Register(0xF00F, 0x8003, Instruction::GetFactoryOf<XOR_R>());
-    decoder.Register(0xF00F, 0x8004, Instruction::GetFactoryOf<ADD_R>());
-    decoder.Register(0xF00F, 0x8005, Instruction::GetFactoryOf<SUB_R>());
-    decoder.Register(0xF00F, 0x8006, Instruction::GetFactoryOf<SHR_R>());
-    decoder.Register(0xF00F, 0x8007, Instruction::GetFactoryOf<SUBN_R>());
-    decoder.Register(0xF00F, 0x800E, Instruction::GetFactoryOf<SHL_R>());
-
-    decoder.Register(0xF00F, 0x9000, Instruction::GetFactoryOf<SNE_R>());
-
-    decoder.Register(0xF000, 0xA000, Instruction::GetFactoryOf<LD_I>());
-
-    decoder.Register(0xF000, 0xB000, Instruction::GetFactoryOf<JMP_V0>());
-
-    decoder.Register(0xF000, 0xC000, Instruction::GetFactoryOf<RND>());
-
-
-
-    Memory mem(romPath);
-    Context ctx;
-
-    /*
-    for(uint32_t i = 0; i < mem.GetInstructionsCount(); i++){
-        uint16_t address = ctx.programCounter.GetAddress();
-        OpCode opcode(mem.Fetch(address));
-        ctx.programCounter.IncrementAddress();
-        auto inst = decoder.Decode(opcode);
-        if(inst)
-            inst->Execute(ctx);
-        else
-            std::cerr << "Unknown opcode: (" << std::hex << std::setfill('0') << 
-            std::setw(4) << opcode.Code() << " at address " << address <<
-            std::dec << std::setfill(' ') << ")" << std::endl;
-    }*/
-
-    std::cout << ctx;
-
-    OpCode opcode(0x4222);
-    auto inst = decoder.Decode(opcode);
-    if(inst)
-        inst->Execute(ctx);
-    else
-        std::cerr << "Unknown opcode: (" << std::hex << std::setfill('0') << 
-        std::setw(4) << opcode.Code() <<
-        std::dec << std::setfill(' ') << ")" << std::endl;
-
-    std::cout << ctx;
-
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
+
+
+    // CLI::App app{"Chip8 Interpreter"};
+    // std::string romPath;
+
+    // app.add_option("rom", romPath, "Caminho da ROM")->required();
+    // app.parse(argc, argv);
+
+    // Decoder decoder;
+    // decoder.Register(0xFFFF, 0x00E0, Instruction::GetFactoryOf<CLS>());
+    // decoder.Register(0xFFFF, 0x00EE, Instruction::GetFactoryOf<RET>());
+
+    // decoder.Register(0XF000, 0x1000, Instruction::GetFactoryOf<JMP>());
+    
+    // decoder.Register(0xF000, 0x2000, Instruction::GetFactoryOf<CALL>());
+    
+    // decoder.Register(0xF000, 0x3000, Instruction::GetFactoryOf<SE_N>());
+
+    // decoder.Register(0xF000, 0x4000, Instruction::GetFactoryOf<SNE_N>());
+
+    // decoder.Register(0xF000, 0x5000, Instruction::GetFactoryOf<SE_R>());
+    
+    // decoder.Register(0xF000, 0x6000, Instruction::GetFactoryOf<LD_N>());
+    
+    // decoder.Register(0xF000, 0x7000, Instruction::GetFactoryOf<ADD_N>());
+
+    // decoder.Register(0xF00F, 0x8000, Instruction::GetFactoryOf<LD_R>());
+    // decoder.Register(0xF00F, 0x8001, Instruction::GetFactoryOf<OR_R>());
+    // decoder.Register(0xF00F, 0x8002, Instruction::GetFactoryOf<AND_R>());
+    // decoder.Register(0xF00F, 0x8003, Instruction::GetFactoryOf<XOR_R>());
+    // decoder.Register(0xF00F, 0x8004, Instruction::GetFactoryOf<ADD_R>());
+    // decoder.Register(0xF00F, 0x8005, Instruction::GetFactoryOf<SUB_R>());
+    // decoder.Register(0xF00F, 0x8006, Instruction::GetFactoryOf<SHR_R>());
+    // decoder.Register(0xF00F, 0x8007, Instruction::GetFactoryOf<SUBN_R>());
+    // decoder.Register(0xF00F, 0x800E, Instruction::GetFactoryOf<SHL_R>());
+
+    // decoder.Register(0xF00F, 0x9000, Instruction::GetFactoryOf<SNE_R>());
+
+    // decoder.Register(0xF000, 0xA000, Instruction::GetFactoryOf<LD_I>());
+
+    // decoder.Register(0xF000, 0xB000, Instruction::GetFactoryOf<JMP_V0>());
+
+    // decoder.Register(0xF000, 0xC000, Instruction::GetFactoryOf<RND>());
+
+
+
+    // Memory mem(romPath);
+    // Context ctx;
+
+    // /*
+    // for(uint32_t i = 0; i < mem.GetInstructionsCount(); i++){
+    //     uint16_t address = ctx.programCounter.GetAddress();
+    //     OpCode opcode(mem.Fetch(address));
+    //     ctx.programCounter.IncrementAddress();
+    //     auto inst = decoder.Decode(opcode);
+    //     if(inst)
+    //         inst->Execute(ctx);
+    //     else
+    //         std::cerr << "Unknown opcode: (" << std::hex << std::setfill('0') << 
+    //         std::setw(4) << opcode.Code() << " at address " << address <<
+    //         std::dec << std::setfill(' ') << ")" << std::endl;
+    // }*/
+
+    // std::cout << ctx;
+
+    // OpCode opcode(0x4222);
+    // auto inst = decoder.Decode(opcode);
+    // if(inst)
+    //     inst->Execute(ctx);
+    // else
+    //     std::cerr << "Unknown opcode: (" << std::hex << std::setfill('0') << 
+    //     std::setw(4) << opcode.Code() <<
+    //     std::dec << std::setfill(' ') << ")" << std::endl;
+
+    // std::cout << ctx;
+
+    // return 0;
 }
