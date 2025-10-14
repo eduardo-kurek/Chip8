fa8d06630cea3d5be8d581a99454349c1fa0d7eb
ViniciusKurek <vinicius.kurek@hotmail.com>
2025-10-13 23:19:29 -0300

    Flag test da instrução 8XY7 passada

--- src/instructions/SUBN_R.cpp
+++ src/instructions/SUBN_R.cpp
@@ -3,12 +3,14 @@
 #include <cstdio>
 
 void SUBN_R::DoExecute(VirtualMachine& vm) const{
-    if (vm.V[opCode.Y()] > vm.V[opCode.X()])
-        vm.V[0xF] = 1;  
-    else
-        vm.V[0xF] = 0;
+    uint16_t sub = vm.V[opCode.Y()] - vm.V[opCode.X()];
+
+    uint8_t notBorrowFlag = (vm.V[opCode.Y()] >= vm.V[opCode.X()]) ? 1 : 0;
+
+    vm.V[opCode.X()] = (uint8_t)sub;
+
+    vm.V[0xF] = notBorrowFlag;
         
-    vm.V[opCode.X()] = vm.V[opCode.Y()] - vm.V[opCode.X()];
 }
 
 std::string SUBN_R::GetName() const{ return "SUBN_R"; }
