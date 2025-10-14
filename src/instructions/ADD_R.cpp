a8d87547905512304f97649747d1972b50d0206c
ViniciusKurek <vinicius.kurek@hotmail.com>
2025-10-13 23:02:33 -0300

    Flag test instrução 8XY4 passada

--- src/instructions/ADD_R.cpp
+++ src/instructions/ADD_R.cpp
@@ -3,13 +3,14 @@
 #include <cstdio>
 
 void ADD_R::DoExecute(VirtualMachine& vm) const{
-    if (vm.V[opCode.X()] + vm.V[opCode.Y()] > 0xFF)
-        vm.V[0xF] = 1;  
-    else
-        vm.V[0xF] = 0;
-
     uint16_t sum = vm.V[opCode.X()] + vm.V[opCode.Y()];
+
+    uint8_t carryFlag = (sum > 0xFF) ? 1 : 0; 
+
     vm.V[opCode.X()] = (uint8_t)sum;
+    
+    vm.V[0xF] = carryFlag;
 }
+
 
 std::string ADD_R::GetName() const{ return "ADD_R"; }
