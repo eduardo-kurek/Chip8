8fd8f997b1991ba5683536f9dbd56cf71634220b
ViniciusKurek <vinicius.kurek@hotmail.com>
2025-10-13 23:45:18 -0300

    Flag test para instrução 8XYE passada

--- src/instructions/SHL_R.cpp
+++ src/instructions/SHL_R.cpp
@@ -3,8 +3,13 @@
 #include <cstdio>
 
 void SHL_R::DoExecute(VirtualMachine& vm) const {
-    vm.V[0xF] = (vm.V[opCode.X()] & 0x80);
+    vm.V[opCode.X()] = vm.V[opCode.Y()];
+
+    uint8_t shiftedBit = vm.V[opCode.X()] & 0x80;
+
     vm.V[opCode.X()] = vm.V[opCode.X()] << 1;
+
+    vm.V[0xF] = shiftedBit >> 7;
 }
 
 std::string SHL_R::GetName() const { return "SHL_R"; }
